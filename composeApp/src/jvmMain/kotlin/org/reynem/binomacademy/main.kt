6743bcc1d20ea3a102df0186e773cd80a7e74b12
Kerim_Lex <lexmeins1@gmail.com>
2025-09-30 20:48:37 +0500

    feat: window size added minimum size

--- composeApp/src/jvmMain/kotlin/org/reynem/binomacademy/main.kt
+++ composeApp/src/jvmMain/kotlin/org/reynem/binomacademy/main.kt
@@ -1,13 +1,19 @@
 package org.reynem.binomacademy
 
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPlacement
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
+import java.awt.Dimension
 
 fun main() = application {
+    val state = rememberWindowState(placement = WindowPlacement.Maximized)
     Window(
+        state = state,
         onCloseRequest = ::exitApplication,
         title = "BinomAcademy",
     ) {
+        window.minimumSize = Dimension(1280, 800)
         App()
     }
 }
