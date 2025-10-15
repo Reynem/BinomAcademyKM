1bce1e653394f2fe0505e8f81fb4a3dc9b64c16a
Kerim_Lex <lexmeins1@gmail.com>
2025-10-15 22:19:15 +0500

    feat: get child data

--- composeApp/src/commonMain/kotlin/org/reynem/binomacademy/viewmodels/TopicIndex.kt
+++ composeApp/src/commonMain/kotlin/org/reynem/binomacademy/viewmodels/TopicIndex.kt
@@ -4,15 +4,26 @@ import org.reynem.binomacademy.data.Topic
 
 class TopicIndex {
     var parentById = mutableMapOf<String, String>()
+    var childById = mutableMapOf<String, MutableList<String>>()
 
     fun buildIndexes(topics: List<Topic>) {
         topics.forEach { topic ->
+            val topicKey = "topic:${topic.id}"
+
             topic.lessons.forEach { lesson ->
-                parentById["lesson:${lesson.id}"] = "topic:${topic.id}"
-                lesson.units.forEach { unitData ->
-                    parentById["unit:${unitData.id}"] = "lesson:${lesson.id}"
-                    unitData.assignments.forEach { assignment ->
-                        parentById["assignment:${assignment.id}"] = "assignment:${unitData.id}"
+                val lessonKey = "lesson:${lesson.id}"
+                parentById[lessonKey] = topicKey
+                childById.getOrPut(topicKey) { mutableListOf() }.add(lessonKey)
+
+                lesson.units.forEach { unit ->
+                    val unitKey = "unit:${unit.id}"
+                    parentById[unitKey] = lessonKey
+                    childById.getOrPut(lessonKey) { mutableListOf() }.add(unitKey)
+
+                    unit.assignments.forEach { assignment ->
+                        val assignmentKey = "assignment:${assignment.id}"
+                        parentById[assignmentKey] = unitKey
+                        childById.getOrPut(unitKey) { mutableListOf() }.add(assignmentKey)
                     }
                 }
             }
