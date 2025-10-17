3141ad9163e254962600dc292a48805551ee6fa1
Kerim_Lex <lexmeins1@gmail.com>
2025-10-17 22:40:56 +0500

    feat: new fields for User

--- composeApp/src/commonMain/kotlin/org/reynem/binomacademy/data/User.kt
+++ composeApp/src/commonMain/kotlin/org/reynem/binomacademy/data/User.kt
@@ -7,6 +7,8 @@ data class User(
     var name: String,
     var avatarPath: String = "",
     var completedUnitsTotal: Int = 0,
+    var completedAssignmentsTotal: Int = 0,
+    var completedAssignments: Set<String> = setOf(),
     var completedUnits: Set<String> = setOf(),
     var achievements: List<String> = listOf(),
     var darkTheme: Boolean = false
